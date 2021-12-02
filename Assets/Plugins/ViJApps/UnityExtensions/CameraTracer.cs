6a9328c60859b13a622caf3a108fdb34d93948ae
ViJ <propellerheadvij@gmail.com>
2021-12-02 23:56:42 +0300

    Added masks to camera tracer

--- Assets/Plugins/ViJApps/UnityExtensions/CameraTracer.cs
+++ Assets/Plugins/ViJApps/UnityExtensions/CameraTracer.cs
@@ -12,7 +12,6 @@ namespace ViJApps
 
         //SETTINGS
         private int mRaycastCapacity;
-        private int mRaycastLayerMask;
 
         //BUFFER DATA
         private RaycastHit[] mHits;
@@ -28,6 +27,79 @@ namespace ViJApps
             }
         }
 
+        #region LAYERS/MASKS
+
+        private int mRaycastLayerMask;
+
+        /// <summary>
+        /// Raycast mask, it is used to filter raycasts with camera
+        /// The default value is -1 that means all layers
+        /// </summary>
+        public int RaycastMask { get; private set; } = -1;
+
+        /// <summary>
+        /// Sets layers to raycast mask
+        /// </summary>
+        /// <param name="layers"></param>
+        public void SetLayers(params string[] layers) => RaycastMask = LayerMask.GetMask(layers);
+
+        /// <summary>
+        /// Adds layers to raycast mask
+        /// </summary>
+        /// <param name="layers"></param>
+        public void AddLayers(params string[] layers) => AddMask(LayerMask.GetMask(layers));
+
+        /// <summary>
+        /// Removes layers from raycast mask
+        /// </summary>
+        /// <param name="layers"></param>
+        public void RemoveLayers(params string[] layers) => RemoveMask(LayerMask.GetMask(layers));
+
+        /// <summary>
+        /// Sets layers to raycast mask
+        /// </summary>
+        /// <param name="layers"></param>
+        public void SetLayers(params int[] layers)
+        {
+            RaycastMask = 0;
+            foreach (var layer in layers)
+                AddMask(1 << layer);
+        }
+
+        /// <summary>
+        /// Adds layers to raycast mask
+        /// </summary>
+        /// <param name="layers"></param>
+        public void AddLayers(params int[] layers)
+        {
+            foreach (var layer in layers)
+                AddMask(1 << layer);
+        }
+
+        /// <summary>
+        /// Removes layers from raycast mask
+        /// </summary>
+        /// <param name="layers"></param>
+        public void RemoveLayers(params int[] layers)
+        {
+            foreach (var layer in layers)
+                RemoveMask(1 << layer);
+        }
+
+        /// <summary>
+        /// Adds bits to raycast mask
+        /// </summary>
+        /// <param name="layers"></param>
+        public void AddMask(int mask) => RaycastMask |= mask;
+
+        /// <summary>
+        /// Removes bits from raycast mask
+        /// </summary>
+        /// <param name="mask"></param>
+        public void RemoveMask(int mask) => RaycastMask &= ~mask;
+
+        #endregion
+
         public CameraTracer(int raycastCapacity)
         {
             RaycastCapacity = raycastCapacity;
@@ -38,12 +110,16 @@ namespace ViJApps
             Array.Resize(ref mHits, mRaycastCapacity);
         }
 
-        public void TraceCamera(Vector2 position, Camera cam)
+        public void TraceCamera(Vector2 position, Camera camera)
         {
-            var ray = cam.ScreenPointToRay(position, Camera.MonoOrStereoscopicEye.Mono);
-            var raycastLimitPlane = new Plane(-cam.transform.forward, cam.transform.position + cam.transform.forward * cam.farClipPlane);
+            var mask = mRaycastLayerMask & camera.cullingMask & ~Physics.IgnoreRaycastLayer;
+            var ray = camera.ScreenPointToRay(position, Camera.MonoOrStereoscopicEye.Mono);
+            var raycastLimitPlane = new Plane(-camera.transform.forward, camera.transform.position + camera.transform.forward * camera.farClipPlane);
             raycastLimitPlane.Raycast(ray, out var rayDistance);
-            mCurrentHitCount = Physics.RaycastNonAlloc(ray, mHits, rayDistance, mRaycastLayerMask);
+            mCurrentHitCount = Physics.RaycastNonAlloc(ray, mHits, rayDistance, mask);
+            if (mCurrentHitCount == mRaycastCapacity)
+                Debug.LogWarning("Hit capacity limit reached, I recommend you to increase mRaycastHitCapacity");
+
             Array.Sort(mHits, 0, mCurrentHitCount, HitDistanceComparer);
         }
 
